7f53dd4c6dcbc700518293ec06dbacf65810ae68
Brandon Himpfen <brandon@himpfen.com>
2015-05-17 20:52:00 -0400

    Initial commit of files.

---
+++ js/app.js
@@ -0,0 +1,13 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // Define your routes
+});
+
+App.IndexRoute = Ember.Route.extend({
+  // Customize the behavior of the route
+  // Example ->
+  // model: function() {
+  //  return ['one', 'two', 'three'];
+  // }
+});
